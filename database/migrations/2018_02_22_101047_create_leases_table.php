937acf18da344e2c650d30a64b7e7de63c6d468a
Tim Joosten <Tjoosten3@gmail.com>
2018-02-24 17:55:11 +0100

    Complete lease database table migration

--- database/migrations/2018_02_22_101047_create_leases_table.php
+++ database/migrations/2018_02_22_101047_create_leases_table.php
@@ -11,10 +11,16 @@ class CreateLeasesTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('leases', function (Blueprint $table) {
             $table->increments('id');
+            $table->boolean('is_confirmed');
+            $table->string('persoon'); 
+            $table->string('email');
+            $table->text('extra_informatie');
+            $table->string('start_datum'); 
+            $table->string('eind_datum');
             $table->timestamps();
         });
     }
@@ -24,7 +30,7 @@ class CreateLeasesTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('leases');
     }
